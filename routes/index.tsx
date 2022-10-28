b045963399cfa2c471fd0835be6875011fae88de
takuyaw-w <takuya.jamtb@gmail.com>
2022-10-28 20:43:18 +0900

    wip

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,24 +1,40 @@
-import { Head } from "$fresh/runtime.ts";
-import Counter from "../islands/Counter.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { getPosts, type Post } from "@/utils/posts.ts";
 
-export default function Home() {
+export const handler: Handlers<Post[]> = {
+  async GET(_req, ctx) {
+    const posts = await getPosts();
+    return ctx.render(posts);
+  },
+};
+
+export default function BlogIndexPage(props: PageProps<Post[]>) {
+  const posts = props.data;
   return (
-    <>
-      <Head>
-        <title>Fresh App</title>
-      </Head>
-      <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <Counter start={3} />
+    <main class="max-w-screen-md px-4 pt-16 mx-auto">
+      <h1 class="text-5xl font-bold">Blog</h1>
+      <div class="mt-8">
+        {posts.map((post) => <PostCard post={post} />)}
       </div>
-    </>
+    </main>
+  );
+}
+
+function PostCard(props: { post: Post }) {
+  const { post } = props;
+  return (
+    <div class="py-8 border(t gray-200)">
+      <a class="block sm:col-span-2" href={`/${post.slug}`}>
+        <h3 class="text(3xl gray-900) font-bold">{post.title}</h3>
+        <time class="text-gray-500">
+          {new Date(post.publishedAt).toLocaleDateString("ja-JP", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </time>
+        <div class="mt-4 text-gray-900">{post.snippet}</div>
+      </a>
+    </div>
   );
 }
---
+++ utils/posts.ts
@@ -0,0 +1,36 @@
+import { extract } from "$std/encoding/front_matter.ts";
+import { join } from "$std/path/posix.ts";
+
+const DIRECTORY = "./posts";
+
+export interface Post {
+  slug: string;
+  title: string;
+  publishedAt: Date;
+  snippet: string;
+  content: string;
+}
+
+export async function getPosts(): Promise<Post[]> {
+  const files = Deno.readDir(DIRECTORY);
+  const promises = [];
+  for await (const file of files) {
+    const slug = file.name.replace(".md", "");
+    promises.push(getPost(slug));
+  }
+  const posts = await Promise.all(promises) as Post[];
+  posts.sort((a, b) => b.publishedAt.getTime() - a.publishedAt.getTime());
+  return posts;
+}
+
+export async function getPost(slug: string): Promise<Post | null> {
+  const text = await Deno.readTextFile(join(DIRECTORY, `${slug}.md`));
+  const { attrs, body } = extract(text);
+  return {
+    slug,
+    title: attrs.title,
+    publishedAt: new Date(attrs.published_at),
+    content: body,
+    snippet: attrs.snippet,
+  };
+}
